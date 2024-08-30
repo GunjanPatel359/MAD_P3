7d107a376ba00551264fc7b1d311310b9fda932a
gunjan patel <g.a.patel359@gmail.com>
2024-08-30 10:18:12 +0530

    hello

--- app/src/main/java/com/example/mad_p3/MainActivity.kt
+++ app/src/main/java/com/example/mad_p3/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mad_p3
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
@@ -24,6 +26,25 @@ class MainActivity : AppCompatActivity() {
         btn1.setOnClickListener {
             val intent=Intent(this,LoginActivity::class.java)
             intent.putExtra("username",edittext.text.toString())
+            startActivity(intent)
+        }
+        val webpagebtn=findViewById<Button>(R.id.webpagebtn)
+        val callbtn=findViewById<Button>(R.id.callbtn)
+        val camerabtn=findViewById<Button>(R.id.camerabtn)
+
+        webpagebtn.setOnClickListener {
+            val intent=Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/GunjanPatel359"))
+            startActivity(intent)
+        }
+
+        callbtn.setOnClickListener {
+            val intent=Intent(Intent.ACTION_DIAL)
+            intent.data=Uri.parse("tel:+9106209714")
+            startActivity(intent)
+        }
+
+        camerabtn.setOnClickListener {
+            val intent=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivity(intent)
         }
     }
